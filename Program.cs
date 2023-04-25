270786edaa283efc2dfb3eeb5c3b513b1f319e14
vichoste <thunderwearsvicho37@protonmail.com>
2023-04-24 21:38:22 -0400

    Pain 5

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<IccDbContext>(options => options.UseSqlServer(defaultConnection));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddIdentity<IccUser, IdentityRole>().AddEntityFrameworkStores<IccDbContext>().AddDefaultTokenProviders();
+builder.Services.Configure<IdentityOptions>(options =>{
+	options.Password.RequireDigit = true;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireUppercase = true;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequiredLength = 6;
+	options.Password.RequiredUniqueChars = 1;
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
